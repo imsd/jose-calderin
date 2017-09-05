ea84adec5612a46cd4913535602962d0f93f76a0
Student <student@ent-v314-008.local>
2017-09-05 15:40:58 -0400

    day 2

---
+++ Day 1/Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	//Let's you change the axis it rotates (x,y,z) in real time.
+	public Vector3 myRotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate (new Vector3 (10, 15, 20) * Time.deltaTime);
+		transform.Rotate (myRotation * Time.deltaTime);
+		
+	}
+}
