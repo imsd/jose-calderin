fd3ffa3cbb2a62f7ab102c7ab06cc94640711e28
Jose Calderin <jose.calderin@mail.citytech.cuny.edu>
2017-12-18 16:32:53 -0500

    Add files via upload
    These are the assets and the scene for having twitch chat play and have get number of viewer that a specific twitch streamer has using api to find it

---
+++ Project 3/Assets/Assets/Twitchapi.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Twitchapi : MonoBehaviour {
+
+	public string url = "http://tmi.twitch.tv/group/user/imaqtpie/chatters";
+	IEnumerator Start() {
+
+		// fetch the actual info, like you would from a browser
+		WWW www = new WWW (url);
+
+		// yield return waits for the download to complete before proceeding
+		// but since this is in IEnumerator it wont stall the program outright
+		yield return www;
+
+		// use a JSON Object to store the info temporarily
+		// this makes it easy to access the data struture
+		JSONObject tempData = new JSONObject (www.text);
+
+
+
+		JSONObject chatters = tempData ["chatters"];
+		JSONObject moderators = chatters ["moderators"];
+		JSONObject viewers = chatters ["viewers"];
+		int viewercount = viewers.Count;
+
+		Debug.Log(moderators[0]);
+        Debug.Log(viewercount.ToString());
+       
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Project 3/Assets/Assets/info.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class info : MonoBehaviour {
+
+
+		// assign the game objects that will be animated
+		public Transform Shape1;
+
+
+		// beginning property for shapes
+		public Vector3 startingValue;
+
+		// ending properties for shapes
+		public Vector3 TargetValue1;
+
+
+		// keep track of time
+		public float AnimationLength = 3f;
+
+		// text object
+		//public TextMesh DisplayText;
+
+		// Use this for initialization
+		void Start () {
+
+			// as an alternative to assigning in the inspector,
+			// assign by finding names of game objects
+			Shape1 = GameObject.Find ("Shape (1)").transform;
+
+
+			//DisplayText = GameObject.Find ("Display Text").GetComponent<TextMesh>();
+
+			// set all scales to starting value
+			Shape1.transform.localScale = startingValue;
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+			// what total is that as a proportion?
+			float Proportion = Time.time / AnimationLength;
+
+			// animate using "Lerp" function which moves between two values smoothly
+			Shape1.transform.localScale = Vector3.Lerp (startingValue, TargetValue1, Proportion);
+
+			// do something with the text display
+			// convert the Proportion (zero to one) to a percentage (0 to 100)
+			int Percentage = (int)Mathf.Round((Proportion * 100));
+			// clamp it so it doesnt exceed 100
+			Percentage = Mathf.Clamp (Percentage, 0, 100);
+			// add a % symbol on the end
+			string PercentageString = Percentage.ToString() + "%";
+			// set the display text to that value
+			//DisplayText.text = PercentageString;
+		}
+	}
